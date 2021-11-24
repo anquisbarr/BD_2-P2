b50f44730a2b5467f4cd184603ad28243fff04da
anquisbarr <sebastian.quispe.b@utec.edu.pe>
2021-11-24 15:33:05 -0500

    Tokenizer with stopwords

---
+++ inverted_index/inverted_index.py
@@ -0,0 +1,21 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+
+# Returns clean tokens from a tweet given
+def stopwords_stemmer(tweet):
+    tokens = nltk.word_tokenize(tweet)
+    stoplist = stopwords.words['spanish']
+    stoplist += []
+    clean_tokens = tokens.copy()
+
+    for token in tokens:
+        if token in stoplist:
+            clean_tokens.remove(token)
+    
+    stemmer = SnowballStemmer("spanish")
+    for i in range(len(clean_tokens)):
+        clean_tokens[i] = stemmer.stem(clean_tokens[i])
+    
+    return clean_tokens
+
